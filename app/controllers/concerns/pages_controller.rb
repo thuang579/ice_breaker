8f38bba703c484a80ab9d9324959cd60c8f93d0b
thuang <thuang579@gmail.com>
2014-11-11 23:14:14 -0800

    showing results

---
+++ app/controllers/concerns/pages_controller.rb
@@ -0,0 +1,9 @@
+class PagesController < ApplicationController
+	def show
+		
+	end
+
+	def results
+
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  get '/' => 'pages#show'
+  get '/results' => 'pages#results'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
